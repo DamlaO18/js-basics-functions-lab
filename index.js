d3e7796f0a7fff86dfd4747f3579862945058555
Damla Osman <damlaaosman@gmail.com>
2022-06-30 20:22:43 +1000

    commit tree b2485418450c8ac9187f61fc63c4d9d47040874f

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    return(Math.abs(distance-42));
+}
+
+function distanceFromHqInFeet(distance) {
+    return(Math.abs(distanceFromHqInBlocks(distance)*264));
+}
+
+function distanceTravelledInFeet(x, y) {
+    return(Math.abs((x - y) * 264));
+}
+
+function calculatesFarePrice(start, destination) {
+    
+    if (Math.abs((start - destination) * 264) <= 400) {
+        return(0);
+    } else if (Math.abs((start - destination) * 264) > 400) {
+        return(Math.abs((start - destination) * 264 -400) * 0.02);
+    } else if ((Math.abs(start - destination) * 264) > 2000) {
+        return(25);
+    } else if (Math.abs((start - destination) * 264) >= 2500) {
+        return('cannot travel that far');
+    } else {
+        return("try again");
+    }
+}
+
+console.log(calculatesFarePrice(50, 58));
+console.log(calculatesFarePrice(34, 24));
